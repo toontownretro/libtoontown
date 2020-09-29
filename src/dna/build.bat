493fcb5abc61c5d30b607c0d16ecc5da92cb48c5
Prince Frizzy <theclashingfritz@users.noreply.github.com>
2020-09-29 19:16:33 -0400

    dna: Rename these files to be properly named.

--- src/dna/build.bat
+++ src/dna/build.bat
@@ -1,4 +1,4 @@
 @echo off
-win_flex -P dnayy -o lexer.cxx lexer.lxx
-win_bison -y -d -p dnayy -o parser.cxx parser.yxx
+win_flex -P dnayy -o lexer.cxx.prebuilt lexer.lxx
+win_bison -y --defines=parser.hxx.prebuilt -p dnayy -o parser.cxx.prebuilt parser.yxx
 pause
